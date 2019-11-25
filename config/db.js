ff891a75041aca11bc1edf08071da5ecab89eb01
Timothy Otieno <timothyking094@gmail.com>
2019-11-25 12:53:11 +0300

    Initialised the Server, the Database and the routing files

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongoURI");
+
+const connectDB = () => {
+  mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connection to the Database Successful...");
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
+};
+
+module.exports = connectDB;
